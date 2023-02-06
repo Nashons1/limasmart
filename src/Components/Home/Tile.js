8d50a2b10179072b8ae8c4a661646f0b644bf6c3
Griffins-sys254 <griffinesonyango@gmail.com>
2023-02-06 11:25:58 +0300

    tile changes

--- src/Components/Home/Tile.js
+++ src/Components/Home/Tile.js
@@ -33,42 +33,42 @@ function Tile({data}) {
 <div className='w-full second'>
      <div className='second-first flex'>
 <div className='px-2 py-1'>
-<p className='font-bold  text-lg md:text-xl okay-bold'>$4000</p>
-<p>Min Invest</p>
+<p className='font-bold  text-lg md:text-xl okay-bold'>$20</p>
+<p>Price</p>
 </div>
 <div style={{borderLeft:'1px solid #000',height:'40px'}} className='grid self-center mr-1'></div>
 
 <div className='px-2 py-1'>
 <p className='font-bold text-lg md:text-xl okay-bold'>${data.target}</p>
-<p>Target</p>
+<p>Bids</p>
 </div>
 <div style={{borderLeft:'1px solid #000',height:'40px'}} className='grid self-center mr-1'></div>
 
 <div className='px-2'>
 <p className='font-bold text-lg md:text-xl okay-bold py-1'> ${data.valuation}</p>
-<p>Valuation</p>
+<p>Market Prediction</p>
  </div>
       </div>
 
  <div className='second-second'>
   <ProgressBar now={60} />
   <div className='progress-bar-div flex justify-between text-sm text-slate-600'>
-    <div>${data.raised_amount} Raised</div>
-    <div>{data.days_left}days left</div>
+    <div>${data.raised_amount} amount used</div>
+    <div>{data.days_left}products left</div>
   </div>
 </div>
     </div>   
     <div className='w-full third'>
       <div className='third-first flex mb-2'>
-      <div className='third-buttons-first px-1'>Real Estate</div>
-     <div className='third-buttons-first ml-2 px-1'>PreferredIncome</div>
+      <div className='third-buttons-first px-1'></div>
+     <div className='third-buttons-first ml-2 px-1'></div>
       </div>
       <div className='third-second flex '>
-         <div className='third-buttons-second px-1'>Income generating</div>
+         <div className='third-buttons-second px-1'></div>
      </div>
     </div>
 
-     <div className='investors flex text-slate-600'><FaUserPlus className='flex self-center' /><p className='ml-2'>{data.investors} Investors</p></div>
+     <div className='investors flex text-slate-600'><FaUserPlus className='flex self-center' /><p className='ml-2'>{data.investors} Supports</p></div>
      </div>
 
    
